34725aecdb606da8954c1707e653b74e6544ec0e
chintanshah2587 <74358132+chintanshah2587@users.noreply.github.com>
2021-10-29 23:09:58 +0530

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -1,9 +1,9 @@
 import sys
 num = len(sys.argv)
 def getSum(num):
-sum = 0
-for i in range(1,num):
-for digit in str(sys.argv[i]):
-sum+=int(digit)
+  sum = 0
+  for i in range(1,num):
+    for digit in str(sys.argv[i]):
+      sum+=int(digit)
 return sum
 print(getSum(num))
